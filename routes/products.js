77ed80893d4db5befd5b81a5995218018baacbcc
Emily Perez <emilynperez2020@gmail.com>
2025-05-16 12:27:10 -0700

    Fixed categories to have their proper title and to have their own page

--- routes/products.js
+++ routes/products.js
@@ -7,14 +7,19 @@ const db = new sqlite3.Database("./data/database.sqlite");
 router.get("/", (req, res) => {
   const category = req.query.category;
   let query = "SELECT * FROM products";
+  let params = [];
+  let title = "Products";
+
   if (category) {
     query += " WHERE category = ?";
+    params.push(category);
+    title = category.charAt(0).toUpperCase() + category.slice(1); // Capitalize
   }
-  db.all(query, category ? [category] : [], (err, products) => {
+
+  db.all(query, params, (err, products) => {
     if (err) return res.status(500).send("DB error.");
-    res.render("products", { products });
+    res.render("products", { products, title });
   });
 });
 
 module.exports = router;
-
